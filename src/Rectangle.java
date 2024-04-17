359c536c19475dbc9cf12b668b09929189b06db5
Garfield <demyanovaya@gmail.com>
2024-04-17 13:53:42 +0200

    Student

---
+++ src/Rectangle.java
@@ -0,0 +1,18 @@
+public class Rectangle {
+
+        int length;
+        int width;
+
+        public Rectangle( int length, int width){
+            this.length = length;
+            this.width = width;
+        }
+
+        int getArea(){
+            return  length * width;
+        }
+
+        int getPerimeter(){
+            return 2 * (length + width);
+        }
+    }
